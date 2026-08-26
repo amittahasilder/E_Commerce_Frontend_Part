c48dabdbe7677017c4aab8a9297f2b077e11b2cf
amittahasilder <amittahasilder@gmail.com>
2026-08-26 05:04:12 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-950">
+      <h1 className="text-4xl font-bold text-white">
+        SHOPX
+      </h1>
+    </div>
+  );
+}
+
+export default App;
